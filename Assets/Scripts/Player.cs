138be9275342a6602b1425f8546ceaeb6d34f45e
Gerardo02 <gerardo.jerez@hotmail.com>
2022-01-25 17:37:12 -0700

    jumpfix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,15 @@ public class Player : MonoBehaviour
     [SerializeField]
     float jumpForce = 7f;
 
+    [SerializeField]
+    float rayDistance = 5f;
+
+    [SerializeField]
+    Color rayColor;
+
+    [SerializeField]
+    LayerMask groundLayer;
+
     Rigidbody2D rb2D; 
 
     void Start()
@@ -21,7 +30,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.right * Axis.x * moveSpeed * Time.deltaTime);
-        if(JumpButton)
+        if(JumpButton && IsGrounding)
         {
             rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
@@ -30,5 +39,14 @@ public class Player : MonoBehaviour
     Vector2 Axis => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
     bool JumpButton => Input.GetButtonDown("Jump");
+
+    bool IsGrounding => Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundLayer);
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position, Vector2.down * rayDistance);
+
+    }
 
 }
